7347e56d6cfea50bd558d5d2ecf3b646a92b141c
fehporto97 <feh.porto97@gmail.com>
2022-09-12 20:49:54 -0300

    criacao da pagina instituicoes e criacao das imagens

--- src/pages/site/Instituicoes.jsx
+++ src/pages/site/Instituicoes.jsx
@@ -1,20 +1,78 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import insti_img from '../../assets/img/banner1.jpg';
-import principal_img from '../../assets/styles/Instituicao.css';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import React from "react";
+import { Container } from "react-bootstrap";
+import insti_img from "../../assets/img/instituicao.png";
+import principal_img from "../../assets/styles/Instituicao.css";
+import Footer from "./components/Footer";
+import Table from "react-bootstrap/Table";
+import { FaInstagram } from "react-icons/fa";
 
 export default function Instituicoes() {
   return (
     <div>
-    <a href='/' className='inicio' >Inicio</a>
-    <Container className="mt-3">
-      <Carousel />
-      <h1 className='titulo' >Titulo Lorem ipsum </h1>
-
+      <Container className="mt-3">
+        <img src={insti_img} className="img_principal" />
+        <h1 className="titulo">
+          Conheca mais sobre as instituicoes parceiras.{" "}
+        </h1>
+        <Table striped bordered hover size="sm" className="tabela-inst">
+          <thead>
+            <tr>
+              <th>Nome da instituicao</th>
+              <th>Cidade</th>
+              <th>Instagram</th>
+              <th>Site</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Apae</td>
+              <td>Bauru - SP</td>
+              <td>
+                <a href="https://www.instagram.com/apaebauru/" target="_blank">
+                  <FaInstagram /> @apaebauru
+                </a>
+              </td>
+              <td>
+                <a href="https://www.apaebauru.org.br/" target="_blanck">
+                  Acessar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Sorri </td>
+              <td>Bauru - SP</td>
+              <td>
+                <a href="https://www.instagram.com/sorribauru/" target="_black">
+                  <FaInstagram /> @sorribauru
+                </a>
+              </td>
+              <td>
+                <a href="https://sorribauru.com.br/" target="_blanck">
+                  Acessar
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Vila Vicentina </td>
+              <td>Bauru - SP</td>
+              <td>
+                <a
+                  href="https://www.instagram.com/vilavicentinabauru/"
+                  targer="_blank"
+                >
+                  <FaInstagram /> @vilavicentinabauru
+                </a>
+              </td>
+              <td>
+                <a href="https://vilavicentinabauru.com.br/" target="_blanck">
+                  Acessar
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
       <Footer />
-    </Container>
     </div>
-  )
+  );
 }
