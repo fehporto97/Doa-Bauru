77a0011e64fd55ecdcd81652ca90e499fbeb0228
fehporto97 <feh.porto97@gmail.com>
2022-09-12 22:18:28 -0300

    alteracoes

---
+++ src/pages/site/QuemSomos.jsx
@@ -0,0 +1,14 @@
+import react from "react-bootstrap";
+
+export default function QuemSomos() {
+  return (
+    <div>
+      <div>dededededede</div>
+      <div>dededededede</div>
+      <div>dededededede</div>
+      <div>dededededede</div>
+      <div>dededededede</div>
+      <div>dededededede</div>
+    </div>
+  );
+}
